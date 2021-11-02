9446833b5c503e0294e3ee098fe06fafd6ad280e
Carly Orr <carly_wong_orr@bcit.ca>
2021-11-01 23:05:01 -0700

    stuff

---
+++ scripts/firebaseAPI_Team99.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBjRa7SX_vvq8B1QIYvBG5lcKCIqTgT2MU",
+  authDomain: "project-1816688544984557349.firebaseapp.com",
+  projectId: "project-1816688544984557349",
+  storageBucket: "project-1816688544984557349.appspot.com",
+  messagingSenderId: "182845258574",
+  appId: "1:182845258574:web:fecc7fa70a41435de07c1f"
+};
+  
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+  
