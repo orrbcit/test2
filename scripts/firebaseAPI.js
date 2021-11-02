0e6af3956b7d2787b40e17b881ddaf61ac02b506
Carly Orr <carly_wong_orr@bcit.ca>
2021-11-01 23:01:21 -0700

    add api

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: ,
-  authDomain: ,
-  projectId: ,
-  storageBucket: ,
-  messagingSenderId: ,
-  appId: 
+  apiKey: "AIzaSyBjRa7SX_vvq8B1QIYvBG5lcKCIqTgT2MU",
+  authDomain: "project-1816688544984557349.firebaseapp.com",
+  projectId: "project-1816688544984557349",
+  storageBucket: "project-1816688544984557349.appspot.com",
+  messagingSenderId: "182845258574",
+  appId: "1:182845258574:web:fecc7fa70a41435de07c1f"
 };
   
 
